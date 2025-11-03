035f44297a4569f20cc04e69061d48a40b78b3a7
Данило <@LAPTOP-7M1VSVI1>
2025-11-03 22:12:19 +0200

    OOP-version of the project 'Guess the number' was created.

---
+++ Guess the number(OOP)/Guess the number(OOP).cpp
@@ -0,0 +1,43 @@
+﻿// OOP-version of project
+#include <iostream>
+#include <random>
+#include <limits>
+void error_check(int& a) {
+    std::cin >> a;
+    if (std::cin.fail()) {
+        do {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Ошибка: введён символ вместо числа. Попробуйте еще раз." << std::endl;
+            std::cin >> a;
+        } while (std::cin.fail());
+    }
+}
+
+class Game {
+private:
+    int rand_num;
+public:
+    Game() {
+        std::random_device seed;
+        std::mt19937 engine(seed());
+        std::uniform_int_distribution<int> dist(1, 50);
+        rand_num = dist(engine);
+    }
+    int get_rand_num() {
+        return rand_num;
+    }
+};
+class Player {
+private:
+    int user_num;
+public:
+};
+
+int main()
+{
+    Game computer;
+    Player player;
+
+    return 0;
+}
